0624f1800cf50f597e0349f5821fca2646510f44
Vishaka Bhaskaran <vishaka.4167@gmail.com>
2025-06-04 15:06:27 -0700

    Update main.c

--- codecrafters-shell-c/src/main.c
+++ codecrafters-shell-c/src/main.c
@@ -8,7 +8,6 @@
 #include <errno.h>
 #include <ctype.h>
 #include <fcntl.h>
-#include <signal.h>
 
 #define CMD_LST_SIZE 5
 #define MAX_ARGS 50
@@ -25,65 +24,59 @@ int is_cmd(char *cmd) {
     return -1;
 }
 
-// Parse input line into arguments, handling quotes and escape characters
+// Parse input line into arguments
 int parse_input(char *input, char *args[], int max_args) {
-  int argc = 0;
-  char *p = input;
+    int argc = 0;
+    char *p = input;
 
-  while (*p && argc < max_args - 1) {
-      // Skip leading whitespace
-      while (isspace((unsigned char)*p)) p++;
-      if (*p == '\0') break;
+    while (*p && argc < max_args - 1) {
+        while (isspace((unsigned char)*p)) p++;
+        if (*p == '\0') break;
 
-      char buffer[1024];
-      int buf_idx = 0;
+        char buffer[1024];
+        int buf_idx = 0;
 
-      // Collect the full argument including any quoted/unquoted segments
-      while (*p && !isspace((unsigned char)*p)) {
-          if (*p == '"' || *p == '\'') {
-              char quote = *p++;
-              while (*p && *p != quote) {
-                  if (quote == '"' && *p == '\\' && *(p + 1)) {
-                      char next = *(p + 1);
-                      if (next == '"' || next == '\\' || next == '$' || next == '`') {
-                          buffer[buf_idx++] = next;
-                          p += 2;
-                      } else {
-                          // Preserve other escapes literally inside double quotes
-                          buffer[buf_idx++] = *p++;
-                      }
-                  } else {
-                      buffer[buf_idx++] = *p++;
-                  }
-              }
-              if (*p == quote) p++; // Skip closing quote
-              else {
-                  fprintf(stderr, "parse_input: missing closing %c quote\n", quote);
-                  for (int i = 0; i < argc; i++) free(args[i]);
-                  return -1;
-              }
-          } else if (*p == '\\' && *(p + 1)) {
-              // Outside quotes: treat \x as x (basic escaping)
-              buffer[buf_idx++] = *(p + 1);
-              p += 2;
-          } else {
-              buffer[buf_idx++] = *p++;
-          }
-      }
+        while (*p && !isspace((unsigned char)*p)) {
+            if (*p == '"' || *p == '\'') {
+                char quote = *p++;
+                while (*p && *p != quote) {
+                    if (quote == '"' && *p == '\\' && *(p + 1)) {
+                        char next = *(p + 1);
+                        if (next == '"' || next == '\\' || next == '$' || next == '`') {
+                            buffer[buf_idx++] = next;
+                            p += 2;
+                        } else {
+                            buffer[buf_idx++] = *p++;
+                        }
+                    } else {
+                        buffer[buf_idx++] = *p++;
+                    }
+                }
+                if (*p == quote) p++;
+                else {
+                    fprintf(stderr, "parse_input: missing closing %c quote\n", quote);
+                    return -1;
+                }
+            } else if (*p == '\\' && *(p + 1)) {
+                buffer[buf_idx++] = *(p + 1);
+                p += 2;
+            } else {
+                buffer[buf_idx++] = *p++;
+            }
+        }
 
-      buffer[buf_idx] = '\0';
-      args[argc] = malloc(buf_idx + 1);
-      if (!args[argc]) {
-          fprintf(stderr, "parse_input: memory allocation failed\n");
-          for (int i = 0; i < argc; i++) free(args[i]);
-          return -1;
-      }
-      strcpy(args[argc], buffer);
-      argc++;
-  }
+        buffer[buf_idx] = '\0';
+        args[argc] = malloc(buf_idx + 1);
+        if (!args[argc]) {
+            fprintf(stderr, "parse_input: memory allocation failed\n");
+            return -1;
+        }
+        strcpy(args[argc], buffer);
+        argc++;
+    }
 
-  args[argc] = NULL;
-  return argc;
+    args[argc] = NULL;
+    return argc;
 }
 
 // Free dynamically allocated arguments
@@ -93,19 +86,9 @@ void free_args(char *args[], int argc) {
     }
 }
 
-void sigint_handler(int sig) {
-    // Ignore Ctrl+C in shell
-    if (isatty(STDIN_FILENO)) {
-        write(STDOUT_FILENO, "\n$ ", 3);
-    }
-}
-
 int main() {
     char input[1024];
-    setbuf(stdout, NULL); // Ensure immediate stdout flush
-
-    // Ignore SIGINT in shell
-    signal(SIGINT, sigint_handler);
+    setbuf(stdout, NULL);
 
     while (1) {
         if (isatty(STDOUT_FILENO)) {
@@ -114,46 +97,80 @@ int main() {
         }
 
         if (fgets(input, sizeof(input), stdin) == NULL) break;
-        input[strcspn(input, "\n")] = '\0';  // Remove newline
+        input[strcspn(input, "\n")] = '\0';
 
         char *args[MAX_ARGS];
         int arg_count = parse_input(input, args, MAX_ARGS);
 
-        if (arg_count <= 0) continue;
-
-        char *outfile = NULL;
-        bool is_redirect = false;
-        int saved_stdout = -1;
-
-        // Detect output redirection
-        for (int i = 0; i < arg_count - 1; i++) {
-            if (strcmp(args[i], ">") == 0 || strcmp(args[i], "1>") == 0) {
-                if (i + 1 >= arg_count || !args[i + 1]) {
-                    fprintf(stderr, "syntax error: expected file after '%s'\n", args[i]);
-                    free_args(args, arg_count);
-                    goto next_loop;
-                }
-                outfile = args[i + 1];
-
-                for (int j = i; j + 2 <= arg_count; j++) {
-                    args[j] = args[j + 2];
-                }
-                arg_count -= 2;
-                is_redirect = true;
-                break;
-            }
+        if (arg_count <= 0) {
+            free_args(args, arg_count);
+            continue;
         }
 
-        if (is_redirect) {
+        char *outfile = NULL, *errfile = NULL;
+        bool redirect_out = false, redirect_err = false;
+        bool append_out = false, append_err = false;
+        int saved_stdout = -1, saved_stderr = -1;
+
+        // Detect output and error redirection
+        for (int i = 0; i < arg_count - 1; i++) {
+            if (strcmp(args[i], ">") == 0 || strcmp(args[i], "1>") == 0) {
+                outfile = args[i + 1];
+                redirect_out = true;
+                append_out = false;
+            } else if (strcmp(args[i], ">>") == 0 || strcmp(args[i], "1>>") == 0) {
+                outfile = args[i + 1];
+                redirect_out = true;
+                append_out = true;
+            } else if (strcmp(args[i], "2>") == 0) {
+                errfile = args[i + 1];
+                redirect_err = true;
+                append_err = false;
+            } else if (strcmp(args[i], "2>>") == 0) {
+                errfile = args[i + 1];
+                redirect_err = true;
+                append_err = true;
+            } else {
+                continue;
+            }
+
+            // Remove redirection operator and target
+            for (int j = i; j + 2 <= arg_count; j++) {
+                args[j] = args[j + 2];
+            }
+            arg_count -= 2;
+            i--;
+        }
+
+        // Set up redirection
+        if (redirect_out) {
             saved_stdout = dup(STDOUT_FILENO);
-            int outfd = open(outfile, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+            int flags = O_CREAT | O_WRONLY | (append_out ? O_APPEND : O_TRUNC);
+            int outfd = open(outfile, flags, 0644);
             if (outfd < 0) {
                 fprintf(stderr, "Error opening file %s: %s\n", outfile, strerror(errno));
                 free_args(args, arg_count);
-                goto next_loop;
+                goto loop_continue;
             }
             dup2(outfd, STDOUT_FILENO);
             close(outfd);
+        }
+
+        if (redirect_err) {
+            saved_stderr = dup(STDERR_FILENO);
+            int flags = O_CREAT | O_WRONLY | (append_err ? O_APPEND : O_TRUNC);
+            int errfd = open(errfile, flags, 0644);
+            if (errfd < 0) {
+                fprintf(stderr, "Error opening file %s: %s\n", errfile, strerror(errno));
+                if (redirect_out && saved_stdout != -1) {
+                    dup2(saved_stdout, STDOUT_FILENO);
+                    close(saved_stdout);
+                }
+                free_args(args, arg_count);
+                goto loop_continue;
+            }
+            dup2(errfd, STDERR_FILENO);
+            close(errfd);
         }
 
         int cmd_type = is_cmd(args[0]);
@@ -220,42 +237,42 @@ int main() {
 
             case 4: // cd
             {
-              char *target;
-              if (arg_count >= 2) {
-                  if (args[1][0] == '~') {
-                      const char *home = getenv("HOME");
-                      if (!home) home = "/";
-                      size_t len = strlen(home) + strlen(args[1]);
-                      target = malloc(len);
-                      if (!target) {
-                          fprintf(stderr, "cd: memory allocation error\n");
-                          break;
-                      }
-                      snprintf(target, len, "%s%s", home, args[1] + 1);
-                  } else {
-                      target = args[1];
-                  }
-              } else {
-                  target = getenv("HOME");
-              }
-              if (!target) {
-                fprintf(stderr, "cd: HOME not set\n");
+                char *target;
+                if (arg_count >= 2) {
+                    if (args[1][0] == '~') {
+                        const char *home = getenv("HOME");
+                        if (!home) home = "/";
+                        size_t len = strlen(home) + strlen(args[1]);
+                        target = malloc(len);
+                        if (!target) {
+                            fprintf(stderr, "cd: memory allocation error\n");
+                            break;
+                        }
+                        snprintf(target, len, "%s%s", home, args[1] + 1);
+                    } else {
+                        target = args[1];
+                    }
+                } else {
+                    target = getenv("HOME");
+                }
+
+                if (!target) {
+                    fprintf(stderr, "cd: HOME not set\n");
+                    break;
+                }
+                if (chdir(target) != 0) {
+                    fprintf(stderr, "cd: %s: %s\n", target, strerror(errno));
+                }
+                if (arg_count >= 2 && args[1][0] == '~') {
+                    free(target);
+                }
                 break;
-            }
-            if (chdir(target) != 0) {
-                fprintf(stderr, "cd: %s: %s\n", target, strerror(errno));
-            }
-            if (arg_count >= 2 && args[1][0] == '~') {
-                free(target); // Only free if it was malloc'ed
-            }
-              break;                          
             }
 
             default: // external command
             {
                 pid_t pid = fork();
                 if (pid == 0) {
-                    signal(SIGINT, SIG_DFL); // Restore default signal handling in child
                     execvp(args[0], args);
                     fprintf(stderr, "%s: command not found\n", args[0]);
                     exit(127);
@@ -268,14 +285,18 @@ int main() {
             }
         }
 
-        if (is_redirect && saved_stdout != -1) {
+        if (redirect_out && saved_stdout != -1) {
             dup2(saved_stdout, STDOUT_FILENO);
             close(saved_stdout);
+        }
+        if (redirect_err && saved_stderr != -1) {
+            dup2(saved_stderr, STDERR_FILENO);
+            close(saved_stderr);
         }
 
         free_args(args, arg_count);
 
-    next_loop:
+    loop_continue:
         continue;
     }
 
